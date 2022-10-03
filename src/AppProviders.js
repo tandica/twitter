976706c92c6fa5c5d51be4ddba87ccc045846c07
tandica <williams.tandica@gmail.com>
2022-10-03 17:58:34 -0400

    set up react query

--- src/AppProviders.js
+++ src/AppProviders.js
@@ -1,6 +1,26 @@
 import React from "react";
 import "./styles/main.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevTools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry(failureCount, error) {
+        if (error.status === 404) return false;
+        else if (failureCount < 2) return true;
+        else return false;
+      },
+    },
+  },
+});
 
 export default function AppProviders({ children }) {
-  return <>{children}</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevTools />
+    </QueryClientProvider>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/main.scss";
+import AppProviders from "./AppProviders";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AppProviders>
+    <App />
+  </AppProviders>,
+  document.getElementById("root")
+);
